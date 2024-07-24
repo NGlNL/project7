73323f959ae07e9ad3f5a6e235b4eb2762e5e2d7
Ilya Semak <ladysemak2001@gmail.com>
2024-07-24 18:21:46 +0300

    homework_12_1

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -4,35 +4,36 @@ import os
 
 
 def test_log_file():
-    if os.path.exists('test_log.txt'):
-        os.remove('test_log.txt')
+    if os.path.exists("test_log.txt"):
+        os.remove("test_log.txt")
 
-    @log(filename='test_log.txt')
+    @log(filename="test_log.txt")
     def test_func_success(a, b):
         return a + b
 
     result = test_func_success(1, 2)
     assert result == 3
 
-    with open('test_log.txt', 'r') as f:
+    with open("test_log.txt", "r") as f:
         line = f.readlines()
         assert len(line) == 1
-        assert line[0].strip() == 'test_func_success ok'
+        assert line[0].strip() == "test_func_success ok"
 
 
 def test_log_error():
-    if os.path.exists('test_log.txt'):
-        os.remove('test_log.txt')
+    if os.path.exists("test_log.txt"):
+        os.remove("test_log.txt")
 
-    @log(filename='test_log.txt')
+    @log(filename="test_log.txt")
     def test_func_error(a, b):
         return a / b
 
     test_func_error(1, 0)
-    with open('test_log.txt', 'r') as f:
+    with open("test_log.txt", "r") as f:
         line = f.readlines()
         assert len(line) == 1
-        assert line[0].strip() == 'test_func_error error: division by zero. Inputs: (1, 0), {}'
+        assert line[0].strip() == "test_func_error error: division by zero. Inputs: (1, 0), {}"
+
 
 def test_log_success_console(capsys):
     @log()
@@ -43,7 +44,8 @@ def test_log_success_console(capsys):
     assert result == 3
 
     captured = capsys.readouterr()
-    assert captured.out == 'func_success_console ok\n'
+    assert captured.out == "func_success_console ok\n"
+
 
 def test_log_error_console(capsys):
     @log()
@@ -52,5 +54,4 @@ def test_log_error_console(capsys):
 
     func_error_console(1, 0)
     captured = capsys.readouterr()
-    assert captured.out == 'func_error_console error: division by zero. Inputs: (1, 0), {}\n'
-
+    assert captured.out == "func_error_console error: division by zero. Inputs: (1, 0), {}\n"
